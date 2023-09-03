c0e7ae4ae419976cbdf1b2b91cbb670a8fa7d30e
s22620 <73877454+s22620@users.noreply.github.com>
2023-09-03 19:52:06 +0200

    Add files via upload

---
+++ DagGola/src/main/java/pl/pjatk/FilLab/Car.java
@@ -0,0 +1,15 @@
+package pl.pjatk.FilLab;
+
+public class Car {
+    private String name;
+
+    public Car(name) {
+        this.name=name();
+        System.out.println("Bean is created here");
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
